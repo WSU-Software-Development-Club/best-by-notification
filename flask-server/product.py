83eb658e7c795765d7df60e9b2d33531b383a8fd
MrThomasLibrary <beethoven@MyLaptop.somethingstupid.org>
2024-10-18 21:22:20 -0700

    Moved product.py to Flask-Server folder

---
+++ flask-server/product.py
@@ -0,0 +1,24 @@
+#import dependinces
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+
+db = SQLAlchemy()
+
+#product class
+class Product(db.Model):
+    __tablename__ = 'products'
+    
+    #class variables
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    expiration_date = db.Column(db.Date, nullable=False)
+    
+    #class init
+    def __init__(self, name, expiration_date):
+        self.name = name
+        self.expiration_date = expiration_date
+
+    #class repr
+    def __repr__(self):
+        return f'<Product {self.name}>'
+
