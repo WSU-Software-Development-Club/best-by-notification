49a978c5f4cef7dd43e9fb2a75e858f76474fe7c
Khang M Bui <khangbui2002@gmail.com>
2024-10-19 20:17:38 -0700

    Created a folder 'routes' specifically for HTTP method routes.

---
+++ flask-server/routes/products.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from db_setup import db
+from product import Product
+from flask import Blueprint, request, jsonify
+
+# Create a Blueprint for products
+products_bp = Blueprint('products', __name__)
+
+# TODO: make a route to add a product:
+
+
+# TODO: Route to fetch all products
+
+
+# TODO: Route to fetch a product by name
+
+
+# TODO: Route to fetch a product by ID
+
+
+# TODO: Route to delete a product by ID
