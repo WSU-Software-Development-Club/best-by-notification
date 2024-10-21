edf4cc2d7c16cdb88eefb356b0eb67310cd1f8cc
Isaac Teo <isaac.teo@wsu.edu>
2024-10-21 13:48:48 -0700

    route to get/fetch all products

--- flask-server/routes/products.py
+++ flask-server/routes/products.py
@@ -10,6 +10,19 @@ products_bp = Blueprint('products', __name__)
 
 
 # TODO: Route to fetch all products
+@products_bp.route('/get_product', methods = ['GET'])
+def get_products():
+    products = Product.query.all()
+    all_products = [x for x in products]
+    dict_products = []
+    for cur_product in all_products:
+        dict = {
+            "id" : cur_product.id,
+            "name" : cur_product.name,
+            "expiration date": cur_product.expiration_date.strftime('%Y-%m-%d')
+        }
+        dict_products.append(dict)
+    return jsonify(dict_products, status=200)
 
 
 # TODO: Route to fetch a product by name
