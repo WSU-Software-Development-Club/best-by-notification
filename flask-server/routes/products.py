56cf32d4aa013eface1f5dd6682bdfa8fe8701b2
MrThomasLibrary <beethoven@MyLaptop.somethingstupid.org>
2024-10-21 22:15:32 -0700

    Added add_product() to products.py

--- flask-server/routes/products.py
+++ flask-server/routes/products.py
@@ -5,8 +5,13 @@ from flask import Blueprint, request, jsonify
 
 # Create a Blueprint for products
 products_bp = Blueprint('products', __name__)
-
+@product_bp.route('/add_product', methods = ['POST']):
 # TODO: make a route to add a product:
+def add_product():
+    data = request.get_json()
+    name = data.get('name')
+    expiration_date = datetime.strptime(data.geet('expiration_date'), '%Y-%m-%d')
+    new_product = product.new_product
 
 
 # TODO: Route to fetch all products
